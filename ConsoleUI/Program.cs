e123c28d389e769df2978c1ca87c24d1bb906eeb
Mircea <mircea.precup1999@gmail.com>
2022-03-03 12:20:21 +0200

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IApplicantModel> applicants = new List<IApplicantModel>
+            {
+                new PersonModel { FirstName = "Mircea", LastName = "Precup" },
+                new ManagerModel { FirstName = "Mihnea", LastName = "Stefan" },
+                new ExecutiveModel { FirstName = "Andreea", LastName = "Iovancea" }
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach (var person in applicants)
+            {
+                employees.Add(person.AccountProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{ emp.FirstName } { emp.LastName }: { emp.EmailAddress } IsManager: { emp.IsManager } IsProgrammer: { emp.IsExecutive }");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
